ff69a5350b03124b9983a56865f20d2a13aaf34e
Rishabh Malviya <rishabh.malviya@gmail.com>
2023-06-26 01:41:09 -0700

    correct bucket creation command

--- {{ cookiecutter.project_name }}/setup.sh
+++ {{ cookiecutter.project_name }}/setup.sh
@@ -1,7 +1,10 @@
 mamba env create -f conda.yaml
 mamba activate {{ cookiecutter.project_name }} && pip install -e .
 
-aws s3api create-bucket --bucket {{ cookiecutter.s3_bucket_name }} --region us-west-1
+aws s3 mb s3://{{ cookiecutter.s3_bucket_name }} --region us-west-1
 
 git init
 git remote add origin {{ cookiecutter.git_repo_ssh_url }}
+git add .
+git commit -m 'cookiecutter skeleton init'
+git push --set-upstream origin master
