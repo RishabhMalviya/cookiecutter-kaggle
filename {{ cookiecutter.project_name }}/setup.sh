e4796b06751977e1d76ee96407c962fd33c615ab
Rishabh Malviya <rishabh.malviya@gmail.com>
2025-02-04 16:46:44 -0800

    switch to poetry

--- {{ cookiecutter.project_name }}/setup.sh
+++ {{ cookiecutter.project_name }}/setup.sh
@@ -1,10 +1,11 @@
-mamba env create -f conda.yaml
-mamba activate {{ cookiecutter.project_name }} && pip install -e .
+pyenv install --skip-existing
+poetry install --with dev
 
 aws s3 mb s3://{{ cookiecutter.s3_bucket_name }} --region us-west-1
 
 git init
-git remote add origin {{ cookiecutter.git_repo_ssh_url }}
 git add .
 git commit -m 'cookiecutter skeleton init'
-git push --set-upstream origin master
+git branch -M main
+git remote add origin {{ cookiecutter.git_repo_ssh_url }}
+git push --set-upstream origin main
